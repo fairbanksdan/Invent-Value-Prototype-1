7137b552ac9b1cfa9e79115290c4f57fc78c7ea6
Dan Fairbanks <fairbanksdan@gmail.com>
2014-08-14 16:51:20 -0700

    initial commit

---
+++ Invent Value Prototype/CreateChallengeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateChallengeViewController.h
+//  Invent Value Prototype
+//
+//  Created by Daniel Fairbanks on 8/14/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateChallengeViewController : UIViewController
+
+@end
---
+++ Invent Value Prototype/CreateViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateViewController.h
+//  Invent Value Prototype
+//
+//  Created by Daniel Fairbanks on 8/13/14.
+//  Copyright (c) 2014 Fairbanksdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateViewController : UIViewController
+
+@end
